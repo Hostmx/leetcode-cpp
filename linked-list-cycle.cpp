cc8bfcc9d4ec43d39179a4eaedb16cc92e9ce730
Wei Dai <anguscupid@gmail.com>
2019-12-31 00:20:10 +0800

    add linked list cycle.

---
+++ linked-list-cycle.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        ListNode* tmp = head;
+        ListNode* tmp2 = head;
+        while(tmp2)
+        {
+            tmp2 = tmp2->next;
+            if(tmp2 == tmp)
+                return true;
+            if(tmp2)
+                tmp2 = tmp2->next;
+            if(tmp2 == tmp)
+                return true;
+            tmp = tmp->next;
+        }
+        return false;
+    }
+};
